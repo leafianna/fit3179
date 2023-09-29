b8f1bed0545ffdf497f3ab90d2c0c369ec141e4a
Amelia <angg0024@student.monash.edu>
2023-09-29 18:08:59 +1000

    graph and html added

--- Assign2/vis1.js
+++ Assign2/vis1.js
@@ -1,4 +1,4 @@
-var vg_1 = "test.json";
+var vg_1 = "Graph1.json";
 vegaEmbed("#fatality_map", vg_1).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
