275022be76d2b5dc53ae590a369dd20bba23334d
Amelia Ng <amelia.ng@bigpond.com>
2023-10-09 14:21:10 +1100

    new

--- Assign2/vis1.js
+++ Assign2/vis1.js
@@ -2,3 +2,8 @@ var vg_1 = "Graph1.json";
 vegaEmbed("#fatality_map", vg_1).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+var vg_2 = "Graph2.json";
+vegaEmbed("#bubble_map", vg_2).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
